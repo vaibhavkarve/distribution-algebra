1f41240100d974e088b382d9c6c884d8fd799fc3
Vaibhav Karve <vkarve@protonmail.com>
2023-01-02 23:14:35 -0500

    add imports of distribution classes

--- distribution_algebra/__init__.py
+++ distribution_algebra/__init__.py
@@ -1,1 +1,6 @@
 #!/usr/bin/env python3
+
+from distribution_algebra.normal import Normal
+from distribution_algebra.beta import Beta
+from distribution_algebra.lognormal import Lognormal
+from distribution_algebra.poisson import Poisson
