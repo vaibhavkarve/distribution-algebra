5e74dd090b4df2d9f26c4ea1fd4f956db6af2c09
Vaibhav Karve <vkarve@protonmail.com>
2023-01-04 09:24:56 -0500

    add UnivariateDistribution and VectorizedDistribution to __init__ for easy importing

--- distribution_algebra/__init__.py
+++ distribution_algebra/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+# pyright: reportUnusedImport=false
 
 from distribution_algebra.normal import Normal
 from distribution_algebra.beta import Beta
 from distribution_algebra.lognormal import Lognormal
 from distribution_algebra.poisson import Poisson
+from distribution_algebra.distribution import UnivariateDistribution, VectorizedDistribution
