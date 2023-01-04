824fb6c541327e71e965d0b3fdc8323dd21480e1
Vaibhav Karve <vkarve@protonmail.com>
2023-01-04 11:02:53 -0500

    add __all__ to __init__

--- distribution_algebra/__init__.py
+++ distribution_algebra/__init__.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
-# pyright: reportUnusedImport=false
 
 from distribution_algebra.normal import Normal
 from distribution_algebra.beta import Beta
 from distribution_algebra.lognormal import Lognormal
 from distribution_algebra.poisson import Poisson
 from distribution_algebra.distribution import UnivariateDistribution, VectorizedDistribution
+
+# Declaring this silences mypy warning on `from distribution_algebra import Beta` etc.
+__all__ = ["Normal", "Lognormal", "Beta", "Poisson", "UnivariateDistribution",
+           "VectorizedDistribution"]
