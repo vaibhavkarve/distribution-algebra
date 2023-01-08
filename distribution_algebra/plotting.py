dcefb5cbe318020a588862190d55de8210801d5d
Vaibhav Karve <vkarve@protonmail.com>
2023-01-08 18:36:35 -0500

    fix plotting of probability mass functions

--- distribution_algebra/plotting.py
+++ distribution_algebra/plotting.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
+from collections import Counter as counter
 from functools import singledispatch
-from typing import Any
+from typing import Any, Counter
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -47,11 +48,20 @@ def plot_vectorized_distribution(
         vdist: VectorizedDistribution[T_in], ax = None, **kwargs: Any) \
         -> tuple[NDArray[np.float64], NDArray[np.float64], list[Polygon]]:
     ax = ax or plt.gca()
+
+    if not vdist.is_continuous:
+        sample_counter: counter[T_in] = counter(vdist.sample)
+        x: NDArray[np.float64] = np.array(list(sample_counter.keys()), dtype=np.float64)
+        heights: NDArray[np.float64] = np.array(
+            list(sample_counter.values()), dtype=np.float64) / len(vdist.sample)
+        return ax.bar(x, heights, alpha=0.25, align="center", width=0.2, **kwargs) # type: ignore
+
+
     number_of_bins: int = min(len(set(vdist.sample)), 100)
     return ax.hist(vdist.sample, bins=number_of_bins, alpha=0.25, density=True,
                    align="left", **kwargs)  # type: ignore
 
-@logger.catch
+
 def plot_all_distributions() -> None:
     plt.xkcd()
     axes: tuple[tuple[plt.Axes, ...], ...]
@@ -62,13 +72,15 @@ def plot_all_distributions() -> None:
     a: Normal = Normal(mean=-5.0, var=9.0)
     b: Normal = Normal(mean=1.0, var=4.0)
     plot(a, ax=ax, label=f"{a}")
-    plot(b, ax=ax, label=f"{b}")
+    b_plot = plot(b, ax=ax, label=f"{b}")
+    b_plot[0].set_label("Prob. density func.")
     ax.legend()
 
     # Lognormal distributions.
     ax = axes[0][1]
     l1: Lognormal = Lognormal(mean=10, var=10.0)
-    plot(l1, ax=ax, label=f"{l1}")
+    l1_plot = plot(l1, ax=ax, label=f"{l1}")
+    l1_plot[0].set_label("Prob. density func.")
     ax.legend()
 
     # Beta distributions.
@@ -76,13 +88,16 @@ def plot_all_distributions() -> None:
     b1: Beta = Beta(alpha=1, beta=1)
     b2: Beta = Beta(alpha=9, beta=3)
     plot(b1, ax=ax, label=f"{b1}")
-    plot(b2, ax=ax, label=f"{b2}")
+    b2_plot = plot(b2, ax=ax, label=f"{b2}")
+    b2_plot[0].set_label("Prob. density func.")
     ax.legend()
 
     # Poisson distributions.
     ax = axes[1][1]
+    ax.set_xticks(range(20))
     p1: Poisson = Poisson(lam=4)
-    plot(p1, ax=ax, label=f"{p1}")
+    p1_plot = plot(p1, ax=ax, label=f"{p1}")
+    p1_plot[0].set_label("Prob. mass func.")
     ax.legend()
 
     plt.suptitle("Plotting various univariate random distributions.")
