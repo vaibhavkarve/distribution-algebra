79319f7275cfb32662f5e9a3294c4890f3152a05
Vaibhav Karve <vkarve@protonmail.com>
2023-01-08 17:29:07 -0500

    silence warning about font-not-found from matplotlib

--- distribution_algebra/__init__.py
+++ distribution_algebra/__init__.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
-from distribution_algebra.normal import Normal
 from distribution_algebra.beta import Beta
+from distribution_algebra.distribution import (UnivariateDistribution,
+                                               VectorizedDistribution)
 from distribution_algebra.lognormal import Lognormal
+from distribution_algebra.normal import Normal
 from distribution_algebra.poisson import Poisson
-from distribution_algebra.distribution import UnivariateDistribution, VectorizedDistribution
+import logging
 
 # Declaring this silences mypy warning on `from distribution_algebra import Beta` etc.
 __all__ = ["Normal", "Lognormal", "Beta", "Poisson", "UnivariateDistribution",
            "VectorizedDistribution"]
+
+logging.getLogger('matplotlib').setLevel(logging.ERROR)
