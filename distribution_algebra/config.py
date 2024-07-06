2553d620418bd30c2943c4d38bc691d85a29c11d
Vaibhav Karve <vkarve@protonmail.com>
2024-07-06 17:33:32 -0400

    make Config class an attrs class

--- distribution_algebra/config.py
+++ distribution_algebra/config.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 import numpy as np
+from attr import define
+from typing import ClassVar
 
 
+@define
 class Config:
-    rng: np.random.Generator = np.random.default_rng(None)  # pyright: ignore
-    sample_size: int = 100_000
-    support_min: float = -20.0
-    support_max: float = +20.0
-    abs_tol: float = 1e-7
+    rng: ClassVar[np.random.Generator] = np.random.default_rng(None)  # pyright: ignore
+    sample_size: ClassVar[int] = 100_000
+    support_min: ClassVar[float] = -20.0
+    support_max: ClassVar[float] = +20.0
+    abs_tol: ClassVar[float] = 1e-7
